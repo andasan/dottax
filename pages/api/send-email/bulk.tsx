8ed93f441c77803fdcaca96bdf66d11b816f726e
Francois Polo <andasan@gmail.com>
2023-02-22 14:49:48 -0800

    fix on the bulk email in compliance with the cloudinary

--- pages/api/send-email/bulk.tsx
+++ pages/api/send-email/bulk.tsx
@@ -1,43 +1,39 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { render } from "@react-email/render";
-import nodemailer from "nodemailer";
-import prisma from "@/lib/prisma";
-import async from "async";
-import path from "path";
-import fs from "fs";
+import { NextApiRequest, NextApiResponse } from 'next';
+import { render } from '@react-email/render';
+import nodemailer from 'nodemailer';
+import prisma from '@/lib/prisma';
+import async from 'async';
+import path from 'path';
+import fs from 'fs';
 
-import EmailTemplate from "@/email/emails/ciccc-t2202";
+import EmailTemplate from '@/email/emails/ciccc-t2202';
 
 type SendBulkEmailReturnType = {
   message: string;
   status: number;
-}
+};
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const studentsEmailList = await prisma.student.findMany({
     where: {
-      batch: Number(req.body.batchNumber)
+      batch: Number(req.body.batchNumber),
     },
     select: {
       id: true,
       email: true,
       firstName: true,
-      studentId: true
-    }
+      studentId: true,
+    },
   });
-
 
   if (studentsEmailList.length > 0) {
     const result = await sendBulkEmail(studentsEmailList);
-    console.log(">>> studentsEmailList: ", result)
-    res.status(200).json({ message: "Email has been sent", status: 250 });
+    console.log('>>> studentsEmailList: ', result);
+    res.status(200).json({ message: 'Email has been sent', status: 250 });
     // const { message, status }: SendBulkEmailReturnType = await sendBulkEmail(studentsEmailList);
     // res.status(status).json({ message, status });
   } else {
-    res.status(200).json({ message: "All students have received an email blast" });
+    res.status(200).json({ message: 'All students have received an email blast' });
   }
 }
 
@@ -46,6 +42,7 @@ type StudentType = {
   email: string;
   firstName: string;
   studentId: string;
+  attachmentPath?: string;
 };
 
 type StudentEmailProps = StudentType[];
@@ -59,9 +56,9 @@ type MailOptionsProps = {
   html?: string;
 };
 
-function sendBulkEmail(studentsEmailList: StudentEmailProps) {
+async function sendBulkEmail(studentsEmailList: StudentEmailProps) {
   //Retrieve a list of emails with attachments
-  const batchEmailWithAttachments = getAttachments(studentsEmailList);
+  const batchEmailWithAttachments = await getAttachments(studentsEmailList);
 
   try {
     const transporter = nodemailer.createTransport({
@@ -69,57 +66,55 @@ function sendBulkEmail(studentsEmailList: StudentEmailProps) {
       port: 587,
       auth: {
         user: process.env.NODEMAILER_USER,
-        pass: process.env.NODEMAILER_PASS
-      }
+        pass: process.env.NODEMAILER_PASS,
+      },
     });
 
     // create a queue with a concurrency of 2
-    const emailQueue = async.queue(
-      (mailOptions: MailOptionsProps, callback) => {
-        const { to, id } = mailOptions;
+    const emailQueue = async.queue((mailOptions: MailOptionsProps, callback) => {
+      const { to, id } = mailOptions;
 
-        // send the email
-        transporter.sendMail(mailOptions, async (error, info) => {
-          if (error) {
-            // handle any errors
-            throw new Error("LOG ERROR: " + error);
-          } else {
-            // email was sent successfully
-            console.info(`Email sent: ${info.response}. Recipient: ${to}`);
+      // send the email
+      transporter.sendMail(mailOptions, async (error, info) => {
+        if (error) {
+          // handle any errors
+          throw new Error('LOG ERROR: ' + error);
+        } else {
+          // email was sent successfully
+          console.info(`Email sent: ${info.response}. Recipient: ${to}`);
 
-            //update the status of the student
-            await prisma.student.update({
-              where: {
-                id: id
-              },
-              data: {
-                status: "sent"
-              }
-            });
-          }
-          // call the callback to indicate that the task is complete
-          callback();
-        });
-      },
-      2
-    );
-
+          //update the status of the student
+          await prisma.student.update({
+            where: {
+              id: id,
+            },
+            data: {
+              status: 'sent',
+            },
+          });
+        }
+        // call the callback to indicate that the task is complete
+        callback();
+      });
+    }, 2);
 
     // send the emails with unique attachments
     batchEmailWithAttachments.forEach((recipient, i) => {
-      const emailHtml = render(<EmailTemplate studentName={recipient.firstName} />, { pretty: true })
+      const emailHtml = render(<EmailTemplate studentName={recipient.firstName} />, {
+        pretty: true,
+      });
       // create the email options
       emailQueue.push({
         id: recipient.id,
-        from: `CICCC <${process.env.EMAIL_FROM || ""}>`,
+        from: `CICCC <${process.env.EMAIL_FROM || ''}>`,
         to: recipient.email,
-        subject: process.env.EMAIL_SUBJECT || "",
+        subject: process.env.EMAIL_SUBJECT || '',
         attachments: [
           {
-            filename: "t2202-fill-21e.pdf",
+            filename: 't2202-fill-21e.pdf',
             path: recipient.attachmentPath,
-            contentType: "application/pdf"
-          }
+            contentType: 'application/pdf',
+          },
         ],
         html: emailHtml,
       });
@@ -128,18 +123,20 @@ function sendBulkEmail(studentsEmailList: StudentEmailProps) {
     const emailAmount = emailQueue.length();
 
     emailQueue.drain(function () {
-      console.log("all items have been processed");
-      return { message: "All emails have been processed", status: 250 };
+      console.log('all items have been processed');
+      return { message: 'All emails have been processed', status: 250 };
     });
 
     emailQueue.error((err) => {
       console.log(err);
       return { message: err.message, status: 500 };
-    })
+    });
 
     return emailQueue.drain();
-    return { message: `Email sending in process...\nNumber of emails to send: ${emailAmount}`, status: 200 };
-
+    return {
+      message: `Email sending in process...\nNumber of emails to send: ${emailAmount}`,
+      status: 200,
+    };
   } catch (err: any) {
     // Handle the error.
     console.log(err);
@@ -148,14 +145,35 @@ function sendBulkEmail(studentsEmailList: StudentEmailProps) {
 }
 
 // type AccType = { [key: string]: string };
-type AccType = { id: number; firstName: string, email: string; attachmentPath: string };
+type AccType = { id: number; firstName: string; email: string; attachmentPath: string };
 type Acc = AccType[];
+
+// const studentReducer = (acc: Acc, student: StudentType) => {
+//   const { id, email, studentId, firstName } = student;
+//   const pdfDirectory = path.join(process.cwd(), "uploads");
+
+//   const pdfPath = path.join(pdfDirectory, studentId + ".pdf");
+//   // const pdfPath = path.join(pdfDirectory, studentId + '-t2202-fill-21e.pdf');
+
+//   if (fs.existsSync(pdfPath)) {
+//     return [...acc, { id, firstName, email, attachmentPath: pdfPath }];
+//   } else {
+//     //file does not exist
+//     throw new Error("Failed to send email. Student's T2202 form doesn't exists");
+//   }
+
+//   return acc;
+// };
+
+// function getAttachments(data: StudentEmailProps) {
+//   return data.reduce(studentReducer, []);
+// }
 
 const studentReducer = (acc: Acc, student: StudentType) => {
   const { id, email, studentId, firstName } = student;
-  const pdfDirectory = path.join(process.cwd(), "uploads");
+  const pdfDirectory = path.join(process.cwd(), 'uploads');
 
-  const pdfPath = path.join(pdfDirectory, studentId + ".pdf");
+  const pdfPath = path.join(pdfDirectory, studentId + '.pdf');
   // const pdfPath = path.join(pdfDirectory, studentId + '-t2202-fill-21e.pdf');
 
   if (fs.existsSync(pdfPath)) {
@@ -168,6 +186,40 @@ const studentReducer = (acc: Acc, student: StudentType) => {
   return acc;
 };
 
-function getAttachments(data: StudentEmailProps) {
-  return data.reduce(studentReducer, []);
+async function getAttachments(data: StudentEmailProps) {
+  // return data.reduce(studentReducer, []);
+  // Example usage
+
+  const sum = await asyncReduce(
+    data,
+    async (acc: Acc, val: StudentType) => {
+      const { id, email, studentId, firstName } = val;
+      const pdfDirectory = path.join(process.cwd(), 'uploads');
+
+      const pdfPath = path.join(pdfDirectory, studentId + '.pdf');
+      // const pdfPath = path.join(pdfDirectory, studentId + '-t2202-fill-21e.pdf');
+
+      if (fs.existsSync(pdfPath)) {
+        return [...acc, { id, firstName, email, attachmentPath: pdfPath }];
+      }
+
+      return [...acc, val]
+
+    },
+    // async (acc: Acc, val: StudentType) => {
+    //   return acc.push({ ...val, attachmentPath: 'test' });
+    // },
+    []
+  );
+
+  console.log("SUM : ", sum.length)
+  return sum;
 }
+
+const asyncReduce = async (array: StudentEmailProps, reducer: any, initialValue: Acc) => {
+  let accumulator = initialValue;
+  for (let i = 0; i < array.length; i++) {
+    accumulator = await reducer(accumulator, array[i]);
+  }
+  return accumulator;
+};
